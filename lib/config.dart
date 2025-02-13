a9c82430769704aa029c04a7365f94f4670daa85
Max <60371404+iaphub@users.noreply.github.com>
2025-02-12 21:15:34 -0600

    iOS/Android SDK updated to 4.6.2

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 class IaphubConfig {
   static const version = "1.5.0";
-  static const iosSDKVersion =  "4.6.0";
-  static const androidSDKVersion = "4.6.0";
+  static const iosSDKVersion =  "4.6.2";
+  static const androidSDKVersion = "4.6.2";
 }
