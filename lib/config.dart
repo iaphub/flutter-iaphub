dcc87a2fd92be110253b6d775f5b71fc93e27a0c
Max <60371404+iaphub@users.noreply.github.com>
2026-07-24 14:00:35 -0400

    Android SDK updated to 5.2.1

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 class IaphubConfig {
   static const version = "2.1.0";
   static const iosSDKVersion =  "4.6.7";
-  static const androidSDKVersion = "5.1.1";
+  static const androidSDKVersion = "5.2.1";
 }
